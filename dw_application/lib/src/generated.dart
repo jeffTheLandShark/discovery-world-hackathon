6f648fd61b08a4e1aebdcea5129260f6b288dd86
Leigh <makenna.goetsch@gmail.com>
2024-11-23 17:51:09 -0600

    update ui

--- dw_application/lib/src/generated.dart
+++ dw_application/lib/src/generated.dart
@@ -5,9 +5,18 @@ void main() {
 }
 
 ThemeData appTheme = ThemeData(
-  primaryColor: Color(0xFFE5407A),
+  colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: Color(0xFFE5407A),
+      onPrimary: Colors.white,
+      secondary: Color(0xFFADD7FF),
+      onSecondary: Colors.black,
+      surface: Colors.white,
+      onSurface: Colors.black,
+      error: Colors.red,
+      onError: Colors.white),
   scaffoldBackgroundColor: Colors.white,
-  textTheme: TextTheme(
+  textTheme: const TextTheme(
     headlineLarge: TextStyle(
       color: Color(0xFFE5407A),
       fontSize: 32,
@@ -24,16 +33,54 @@ ThemeData appTheme = ThemeData(
   ),
 );
 
-class FigmaToCodeApp extends StatelessWidget {
+class FigmaToCodeApp extends StatefulWidget {
   const FigmaToCodeApp({super.key});
+
+  @override
+  _FigmaToCodeAppState createState() => _FigmaToCodeAppState();
+}
+
+class _FigmaToCodeAppState extends State<FigmaToCodeApp> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: appTheme,
       home: Scaffold(
-        body: Landing(),
-        drawer: Popup(),
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: [
+            Landing(),
+            Popup(),
+            Container(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Exhibits',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.nfc),
+              label: 'Scan NFC',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            )
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
@@ -43,145 +90,90 @@ class Popup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Center(
-        child: Container(
-          height: 1580,
-          width: 720,
-          child: Stack(
-            children: [
-              Positioned(
-                top: 0,
-                left: 0,
-                child: Container(
-                  color: Colors.white,
-                  height: 1580,
-                  width: 720,
-                ),
-              ),
-              Positioned(
-                top: 1,
-                left: -13,
-                child: Container(
-                  height: 2270,
-                  width: 1194,
-                  color: Colors.transparent,
-                ),
-              ),
-              Positioned(
-                top: 0,
-                left: 13,
-                child: Container(
-                  height: 144,
-                  width: 720,
-                  color: Colors.grey, // Placeholder for rectangle1
-                ),
-              ),
-              Positioned(
-                top: 17,
-                left: 34,
-                child: Image.asset(
-                  'assets/discovery-world-2-1.png',
-                  height: 101,
-                  width: 289,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                top: 296,
-                left: 39,
-                child: Container(
-                  height: 887,
-                  width: 658,
-                  color: Color(0xFFADD7FF),
-                ),
-              ),
-              Positioned(
-                top: 351,
-                left: 80,
-                child: Container(
-                  height: 787,
-                  width: 586,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/image.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 1232,
-                left: 0,
-                child: Container(
-                  height: 359,
-                  width: 733,
-                  child: Image.asset(
-                    'assets/curious-beep.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 1501,
-                left: 342,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment.center,
-                        color: Color(0xFFEAEAEF),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'English',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            fontFamily: 'Inter',
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Color(0xFFEAEAEF)),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'Español',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            fontFamily: 'Inter',
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Color(0xFFEAEAEF)),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'HMoob',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            fontFamily: 'Inter',
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/discovery-world-2-1.png',
+            fit: BoxFit.cover,
           ),
-        ),
+          Image.asset(
+            'assets/curious-beep.png',
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 1501,
+            left: 342,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: Color(0xFFEAEAEF),
+                    padding: EdgeInsets.all(10),
+                    child: Text('English'),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFEAEAEF)),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text('Español'),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFEAEAEF)),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text('HMoob'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 1501,
+            left: 342,
+            child: Container(
+              height: 50,
+              child: ToggleButtons(
+                borderColor: Color(0xFFEAEAEF),
+                fillColor: Color(0xFFE5407A),
+                borderWidth: 2,
+                selectedBorderColor: Color(0xFFE5407A),
+                selectedColor: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('English'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('Español'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text('HMoob'),
+                  ),
+                ],
+                onPressed: (int index) {
+                  // Handle button selection
+                },
+                isSelected: [
+                  true,
+                  false,
+                  false
+                ], // Update this list based on the selected language
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -191,85 +183,27 @@ class Landing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            color: Colors.white,
-            height: 1580,
-            width: 720,
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 1,
-                  left: 15,
-                  child: Container(
-                    height: 144,
-                    width: 720,
-                    color: Colors.grey, // Placeholder for rectangle1
-                  ),
-                ),
-                Positioned(
-                  top: 18,
-                  left: 36,
-                  child: Image.asset(
-                    'assets/discovery-world-2-1.png',
-                    height: 101,
-                    width: 289,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  top: 199,
-                  left: 62,
-                  child: Text(
-                    'INTERACTIVE MAP',
-                    style: TextStyle(
-                      color: Color(0xFFE5407A),
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 0.32,
-                      height: 2,
-                      fontFamily: 'Verdana',
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 352,
-                  left: 82,
-                  child: Container(
-                    height: 787,
-                    width: 586,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/image.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  child: IconButton(
-                    icon: Icon(Icons.menu),
-                    onPressed: () {},
-                  ),
-                ),
-                Positioned(
-                  top: 1144,
-                  left: 27,
-                  child: Image.asset(
-                    'assets/worker-beep-and-boop.png',
-                    height: 436,
-                    width: 696,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            ),
+      body: ListView(
+        children: <Widget>[
+          Image.asset(
+            'assets/discovery-world-2-1.png',
+            fit: BoxFit.cover,
           ),
-        ),
+          Text(
+            'INTERACTIVE MAP',
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          Container(
+            // put interactive map here
+            // get device height
+            height: MediaQuery.of(context).size.height * 0.9,
+            color: Colors.grey,
+          ),
+          Image.asset(
+            'assets/images/website_bb.png',
+            fit: BoxFit.cover,
+          ),
+        ],
       ),
     );
   }
