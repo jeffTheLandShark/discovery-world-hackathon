96f79237afa1350ae111788dc21039f7f4226840
roegnere <roegnere@msoe.edu>
2024-11-22 19:05:59 -0600

    Map starts centered

--- dw_application/lib/src/mapping/main_map.dart
+++ dw_application/lib/src/mapping/main_map.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 Future<String> loadAsset() async {
@@ -14,12 +12,19 @@ class MainMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
+      heightFactor: MediaQuery.sizeOf(context).height,
       child: InteractiveViewer(
         constrained: false,
-        boundaryMargin: const EdgeInsets.all(double.infinity),
+        boundaryMargin: const EdgeInsets.all(20),
         minScale: 0.1,
-        maxScale: 1.6,
-        child: const Image(image: AssetImage('assets/images/map_assets/tech_floor2.png'))
+        maxScale: 5,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Center(
+            child: const Image(image: AssetImage('assets/images/map_assets/tech_floor2.png'))
+            ),
+        )
       ),
     );
   }
