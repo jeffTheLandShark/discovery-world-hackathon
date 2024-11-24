853a94585e7b6b8ed620868da7fe283b829bf107
Leigh <makenna.goetsch@gmail.com>
2024-11-24 02:20:00 -0600

    all nodes in same place

--- dw_application/lib/src/mapping/map_node.dart
+++ dw_application/lib/src/mapping/map_node.dart
@@ -1,10 +1,37 @@
 import 'floor_map.dart';
 
+class FloorTransitionNode extends MapNode {
+  String type;
+  List<FloorTransitionNode> canGoTo = [];
+
+  FloorTransitionNode(
+      {required super.floor,
+      required super.xPos,
+      required super.yPos,
+      required this.canGoTo,
+      this.type = "Stairs"});
+
+  List<FloorTransitionNode> get getCanGoTo => canGoTo;
+  String get getType => type;
+}
+
+class ExhibitNode extends MapNode {
+  String id;
+  String description;
+
+  ExhibitNode(
+      {required super.floor,
+      required super.xPos,
+      required super.yPos,
+      required this.id,
+      required this.description});
+}
+
 class MapNode {
   FloorMap _floor;
   double _xPos;
   double _yPos;
-  
+
   MapNode({required FloorMap floor, required double xPos, required double yPos})
       : _floor = floor,
         _xPos = xPos,
