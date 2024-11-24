88572e80978ed113ee9497a3b6d8d5a131377baf
Leigh <makenna.goetsch@gmail.com>
2024-11-23 23:05:00 -0600

    map page no scroll

--- dw_application/lib/src/map_page.dart
+++ dw_application/lib/src/map_page.dart
@@ -18,23 +18,15 @@ class MapView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          Text(
-            'INTERACTIVE MAP',
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-          Container(
-            // put interactive map here
-            // get device height
-            height: MediaQuery.of(context).size.height * 0.9,
-            child: ExhibitPopup(
-              mainKey: passedKey,
-              exhibits: exhibits,
-              exhibitMapEntries: exhibitMapEntries,
-            ),
-          ),
-        ],
+      body: Container(
+        // put interactive map here
+        // get device height
+        height: MediaQuery.of(context).size.height * 0.9,
+        child: ExhibitPopup(
+          mainKey: passedKey,
+          exhibits: exhibits,
+          exhibitMapEntries: exhibitMapEntries,
+        ),
       ),
     );
   }
